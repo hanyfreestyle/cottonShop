19b3280b34432c0c037d2b4deb38746a3b8c2c6b
Hany Darwish <hany.freestyle4u@gmail.com>
2024-07-28 20:39:59 +0300

    Update

--- resources/views/web/layouts/app_soft.blade.php
+++ resources/views/web/layouts/app_soft.blade.php
@@ -31,22 +31,22 @@
     {!! (new \App\Helpers\MinifyTools)->MinifyCss('css/cart_view.css',"Seo",$cssReBuild) !!}
     {!! (new \App\Helpers\MinifyTools)->MinifyCss('css/style_lang_'.thisCurrentLocale().'.css',"Seo",$cssReBuild) !!}
     @livewireStyles
-<!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQBEXFVS4Y"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-        gtag('config', 'G-PQBEXFVS4Y');
+<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PG7D4NS');
     </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="{{htmlBodyStyle($pageView)}}">
 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG7D4NS"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="nt_wrapper">
     <header id="ntheader" class="ntheader header_3 h_icon_la">
