feb5ead6512ae1511016387e0b9a5f2a8436d688
Hany Darwish <hany.freestyle4u@gmail.com>
2024-09-13 11:25:55 +0300

    update

--- resources/views/AppPlugin/ConfigPrivacy/index.blade.php
+++ resources/views/AppPlugin/ConfigPrivacy/index.blade.php
@@ -1,20 +1,20 @@
 @extends('admin.layouts.app')
 @section('StyleFile')
-    <x-admin.data-table.plugins  :style="true" :is-active="$viewDataTable"/>
+    <x-admin.data-table.plugins :style="true" :is-active="$viewDataTable"/>
 @endsection
 
 @section('content')
     <x-admin.hmtl.breadcrumb :pageData="$pageData"/>
     <x-admin.hmtl.section>
 
-            <div class="row mb-3">
-                <div class="col-12 text-left">
-                    <x-admin.form.action-button  url="{{route($PrefixRoute.'.Sort')}}" type="sort" :tip="false" />
-                </div>
+        <div class="row mb-3">
+            <div class="col-12 text-left">
+                <x-admin.form.action-button url="{{route($PrefixRoute.'.Sort')}}" type="sort" :tip="false"/>
             </div>
+        </div>
 
 
-        <x-admin.card.def :page-data="$pageData" >
+        <x-admin.card.def :page-data="$pageData">
 
             @if(count($rowData)>0)
                 <div class="card-body table-responsive p-0">
@@ -26,7 +26,7 @@
                             <th>H1</th>
 
                             @if($pageData['ViewType'] == 'deleteList')
-                                <x-admin.table.soft-delete />
+                                <x-admin.table.soft-delete/>
                             @else
                                 <th></th>
                                 <x-admin.table.action-but po="top" type="edit"/>
@@ -38,15 +38,15 @@
                         @foreach($rowData as $row)
                             <tr>
                                 <td>{{$row->id}}</td>
-                                <td>{{$row->translate('ar')->h1}}</td>
-                                <td>{{$row->translate('en')->h1}}</td>
+                                <td>{{$row->translate('ar')->h1 ?? null}}</td>
+                                <td>{{$row->translate('en')->h1 ?? null }}</td>
 
                                 @if($pageData['ViewType'] == 'deleteList')
-                                    <x-admin.table.soft-delete type="b" :row="$row" />
+                                    <x-admin.table.soft-delete type="b" :row="$row"/>
                                 @else
-                                    <td class="tc" >{!! is_active($row->is_active) !!}</td>
-                                    <x-admin.table.action-but type="edit" :row="$row" />
-                                    <x-admin.table.action-but type="delete" :row="$row" />
+                                    <td class="tc">{!! is_active($row->is_active) !!}</td>
+                                    <x-admin.table.action-but type="edit" :row="$row"/>
+                                    <x-admin.table.action-but type="delete" :row="$row"/>
                                 @endif
                             </tr>
                         @endforeach
@@ -54,15 +54,15 @@
                     </table>
                 </div>
             @else
-                <x-admin.hmtl.alert-massage type="nodata" />
+                <x-admin.hmtl.alert-massage type="nodata"/>
             @endif
         </x-admin.card.def>
-        <x-admin.hmtl.pages-link :row="$rowData" />
+        <x-admin.hmtl.pages-link :row="$rowData"/>
 
     </x-admin.hmtl.section>
 @endsection
 
 @push('JsCode')
     <x-admin.table.sweet-delete-js/>
-    <x-admin.data-table.plugins  :jscode="true" :is-active="$viewDataTable" />
+    <x-admin.data-table.plugins :jscode="true" :is-active="$viewDataTable"/>
 @endpush
